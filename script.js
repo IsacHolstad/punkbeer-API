aca3f70a5622085d05c75d5441a8c0ebaf4341a4
IsacHolstad <wisac22@gmail.com>
2022-05-09 23:51:42 +0200

    for loop on data

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const corsFixUrl = proxy + myAPI;
 console.log(corsFixUrl)
 
 const resultContainer = document.querySelector(".container");
+
+async function results() {
+    try{
+        const response = await fetch(corsFixUrl);
+        console.log(response);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        const beerData = responseJSON.data;
+        for (let i = 0; i < responseJSON.length; i++){
+            console.log(responseJSON[i].name)
+        }
+    }
+    catch(error){
+        resultContainer.innerHTML += `<p>404</p>`;
+    }
+
+}
+results();
