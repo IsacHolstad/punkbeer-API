1faadd62c0f6605deab7d6334c68b61b4f061669
IsacHolstad <wisac22@gmail.com>
2022-05-10 16:21:51 +0200

    pretier code

--- detail.js
+++ detail.js
@@ -1,10 +1,25 @@
-//const myAPI = `https://api.punkapi.com/v2/beers/${id}`;
-//const queryString = document.location.search;
-//const params = new URLSearchParams(queryString);
-//console.log(params);
-//const id = params.get('id');
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+console.log(params);
+const id = params.get('id');
+const myAPI = `https://api.punkapi.com/v2/beers/${id}`;
 
-//const proxy = "https://noroffcors.herokuapp.com/";
-//const corsFixUrl = proxy + myAPI;
-//console.log(corsFixUrl)
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + myAPI;
+console.log(corsFixUrl);
+
+const beerDetails = document.querySelector(".container");
+
+async function beerResults(){
+    try{
+        const response = await fetch(corsFixUrl);
+        const beerData = await response.json();
+        console.log(beerData)
+    }
+    catch(error) {
+        beerDetails.innerHTML = `<p>404</p>`
+    }
+}
+beerResults()
+
 
